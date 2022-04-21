062f2a37cbcc350752e73fe085a0ca38325c1328
NanaValencia <diseno.1988@gmail.com>
2022-04-21 11:20:12 -0500

    Realizamos revisón al código, eliminamos carpeta lib y agregamos mocks

--- src/firebase/__mocks__/firebaseImport.js
+++ src/firebase/__mocks__/firebaseImport.js
@@ -4,3 +4,6 @@ export const createUserWithEmailAndPassword = () => Promise.resolve({});
 export const signInWithEmailAndPassword = () => Promise.resolve({});
 export const signInWithGoogle = () => Promise.resolve({});
 export class GoogleAuthProvider {}
+export const signInWithPopup = () => Promise.resolve({});
+export const signOut = () => Promise.resolve({});
+export const getFirestore = () => Promise.resolve({});
